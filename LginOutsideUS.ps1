e59794b541a52eb485c1721c3d6885dca6e1e69f
Aiden-Chubet <147650530+Aiden-Chubet@users.noreply.github.com>
2024-02-16 14:15:02 -0500

    Create LginOutsideUS.ps1

---
+++ LginOutsideUS.ps1
@@ -0,0 +1,4 @@
+Install-module -Name AzureADpreview
+Connect-AzureAD
+
+Get-AzureADAuditSignInLogs -Filter "Location/CountryorRegion ne 'US'" -All $true | Select CreatedDateTime, UserPrincipalName, IsInteractive, AppDisplayName, IpAddress, TokenIssuerType, @{Name = 'DeviceOS'; Expression = {$_.DeviceDetail.OperatingSystem}}, @{Name = 'City'; Expression = {$_.Location.City}},  @{Name = 'State'; Expression = {$_.Location.State}},  @{Name = 'CountryOrRegion'; Expression = {$_.Location.CountryOrRegion}} | Export-CSV "C:\Exports\AzureADAuditSignInLogs.CSV" -NoTypeInformation -Encoding UTF8
